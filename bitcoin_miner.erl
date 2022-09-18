eba242fac600533a47e39612cae479c4359ff5f5
girishmallya123 <girishm60@gmail.com>
2022-09-18 14:04:41 -0400

    mining with actor model

--- bitcoin_miner.erl
+++ bitcoin_miner.erl
@@ -1,7 +1,7 @@
 -module(bitcoin_miner).
 -import(lists,[nth/2]).
--import(string,[left/3]).
--export([main/1]).
+-import(string,[left/3, concat/2]).
+-export([main/1 , start_mine/2]).
 
 prefix([], _) -> true;
     prefix([Ch | Rest1], [Ch | Rest2]) ->
@@ -15,24 +15,47 @@ get_random_string(Length, AllowedChars) ->
                             ++ Acc
                 end, [], lists:seq(1, Length)).
 
-mine(0, false) ->
+mine(_, false, Pid) ->
+    send_message(Pid, {stop, Pid}),
+    start_mine(0, false),
     ok;
 
-mine(I, Flag) ->
-    String_Characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-    Random_String = get_random_string(32, String_Characters),
-    Hashed_String =io_lib:format("~64.16.0b", [binary:decode_unsigned(crypto:hash(sha256, Random_String))]),
-    Prefix = left("",I,$0), 
-    Status = prefix(Prefix, Hashed_String),
-    if 
-        Status == true ->
-            io:format("Found a bitcoin!~n"),
-            mine(0, false);
-        true ->
-            io:format("Mining~n"),
-            mine(I, Flag)
+mine(I, Flag, Pid) ->
+            String_Characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+            Random_String = get_random_string(32, String_Characters),
+            Input_String = concat("girish.mallya", Random_String),
+            Hashed_String =io_lib:format("~64.16.0b", [binary:decode_unsigned(crypto:hash(sha256, Input_String))]),
+            Prefix = left("",I,$0), 
+            Status = prefix(Prefix, Hashed_String),
+            if 
+                Status == true ->
+                    io:format("~s\t~s~n", [Input_String, Hashed_String]),
+                    mine(I, false, Pid);
+                true ->
+                    mine(I, Flag, Pid)
+            end.
+            
+send_message(Pid, Msg) ->
+    Pid ! Msg.
+
+start_mine(I, Flag) ->
+    receive 
+        {start, S} ->
+            mine(I, Flag, S);
+        {stop, _} ->
+            exit(0)
     end.
-    
+
+spawn_miners(I, 0) ->
+    io:format("~w~n",[I]),
+    ok;
+
+spawn_miners(I, K) ->
+    io:format("Spawning miner number ~w~n", [K]),
+    S = spawn(bitcoin_miner, start_mine, [I, true]),
+    send_message(S, {start, S}),
+    spawn_miners(I, K-1).
+
 main(Args) ->
     N = hd(Args),
     I = list_to_integer(atom_to_list(N)),
@@ -44,4 +67,6 @@ main(Args) ->
         true ->
             io:format("Required number of leading 0's: ~w~n", [N])
     end,
-    mine(I, true).
+
+    spawn_miners(I, 10),
+    timer:sleep(30000).
